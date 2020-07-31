8f1802540f1412cb2dfb9de1077af977ad0dc5de
Shashwat Sharma <shashwatsharma575@gmail.com>
2020-07-31 14:51:12 +0530

    Update particles.js

--- particles.js
+++ particles.js
@@ -20,10 +20,10 @@ var pJS = function(tag_id, params){
     },
     particles: {
       number: {
-        value: 30,
+        value: 400,
         density: {
           enable: true,
-          value_area: 2000
+          value_area: 800
         }
       },
       color: {
